a95b94b56fe72ab4dad77a6c5eb6ac4cfc39eb2e
Cedric VICO <cedric.vico@hotmail.com>
2021-04-29 16:54:57 +0200

    setup

---
+++ Ghost Hat/Assets/Scripts/PropsBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropsBehaviour : MonoBehaviour
+{
+    public bool presence = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
