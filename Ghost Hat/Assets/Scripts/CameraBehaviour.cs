6632ab02c6070037d958bd4ae92e2816299dfb5a
Cedric VICO <cedric.vico@hotmail.com>
2021-05-06 12:02:07 +0200

    setup

--- Ghost Hat/Assets/Scripts/CameraBehaviour.cs
+++ Ghost Hat/Assets/Scripts/CameraBehaviour.cs
@@ -5,64 +5,67 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    Vector2[,] tabSalle;
+    Transform[,] tabSalle;
     public List<SalleData> salles = new List<SalleData>();
     int currentX = 0;
     int currentY = 0;
     int nbSalleX;
     int nbSalleY;
-    int k = 0;
-    Vector2 vector2 = new Vector2();
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        nbSalleX = salles.Count/2 + 1;
-        nbSalleY = salles.Count / 2 ;
-        tabSalle = new Vector2[nbSalleY, nbSalleX];
+        nbSalleX = salles.Count/2 +1;
+        nbSalleY = salles.Count / 2+1 ;
+        tabSalle = new Transform[nbSalleX, nbSalleY];
         for (int i = 0; i < nbSalleY; i++)
         {
             for (int j = 0; j < nbSalleX; j++)
             {
-                tabSalle[i, j] = salles[k].position.position;
-                k += 1;
+                tabSalle[j, i] = null;
             }
+        }
+        foreach(SalleData salleData in salles)
+        {
+            tabSalle[salleData.posX, salleData.posY] = salleData.position;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyUp(KeyCode.UpArrow))
         {
-            if(currentY < nbSalleY)
+            if(currentY+1 < nbSalleY)
             {
                 currentY++;
             }
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             if (currentY > 0)
             {
                 currentY--;
             }
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyUp(KeyCode.RightArrow))
         {
-            if(currentX < nbSalleX)
+            if(currentX+1 < nbSalleX)
             {
                 currentX++;
             }
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             if (currentX > 0)
             {
                 currentX--;
             }
         }
-        Camera.main.transform.position = tabSalle[currentY, currentX];
+        if (tabSalle != null)
+        {
+            Camera.main.transform.position = tabSalle[currentX, currentY].position;
+        }
     }
 }
 [Serializable]
