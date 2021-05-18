33371ee07a9db67279325e0393125d80e5af62f0
Alexandre Le Bacquer <alexandre.lbac@gmail.com>
2021-05-18 10:28:58 +0200

    Radar fini

--- Ghost Hat/Assets/Scripts/Radar.cs
+++ Ghost Hat/Assets/Scripts/Radar.cs
@@ -7,23 +7,15 @@ public class Radar : Item
     public Sprite radar;
     public Sprite radarGhost;
     private Transform ghostObject;
-    SpriteRenderer spriteRenderer;
-
-    void Start()
-    {
-        if (GameObject.FindGameObjectWithTag("Oreille"))
-        {
-            this.ghostObject = GameObject.FindGameObjectWithTag("Oreille").transform;
-        }
-    }
+    public SpriteRenderer spriteRenderer;
 
     void Update()
     {
         Mouse(0.05f, 0);
-        if (ghostObject != null)
+        if (GameObject.FindGameObjectWithTag("Oreille"))
         {
+            this.ghostObject = GameObject.FindGameObjectWithTag("Oreille").transform;
             float distToPlayer = Vector2.Distance(transform.position, ghostObject.position);
-
             if (distToPlayer < 1)
             {
                 spriteRenderer.sprite = radarGhost;
--- Ghost Hat/Assets/Scripts/Wave.cs
+++ Ghost Hat/Assets/Scripts/Wave.cs
@@ -7,18 +7,13 @@ public class Wave : Item
     public Animator anim;
     private Transform ghostObject;
 
-    void Start()
-    {
-        if (GameObject.FindGameObjectWithTag("Oreille"))
-        {
-            this.ghostObject = GameObject.FindGameObjectWithTag("Oreille").transform;
-            Debug.Log("a");
-        }
-    }
-
     void Update()
     {
         Mouse(-0.02f, 1);
+        if (GameObject.FindGameObjectWithTag("Oreille"))
+        {
+            this.ghostObject = GameObject.FindGameObjectWithTag("Oreille").transform;
+        }
         if (ghostObject != null)
         {
             float distToPlayer = Vector2.Distance(transform.position, ghostObject.position);
