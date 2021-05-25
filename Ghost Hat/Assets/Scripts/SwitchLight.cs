c0e6170172b4776d114cf4e5da91a2a966ecb1a9
Cedric VICO <cedric.vico@hotmail.com>
2021-05-25 15:15:59 +0200

    Setup

--- Ghost Hat/Assets/Scripts/SwitchLight.cs
+++ Ghost Hat/Assets/Scripts/SwitchLight.cs
@@ -6,11 +6,14 @@ public class SwitchLight : MonoBehaviour
 {
     public GameObject LightOff;
     public Salle salle;
+    public SpriteRenderer spriteRenderer;
+    public Sprite allumer;
+    public Sprite eteins;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        LightOff.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,11 +26,13 @@ public class SwitchLight : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0) && LightOff.activeSelf)
         {
+            spriteRenderer.sprite = allumer;
             LightOff.SetActive(false);
             salle.lumiere = true;
         }
         else if (Input.GetMouseButtonUp(0) && !LightOff.activeSelf)
         {
+            spriteRenderer.sprite = eteins;
             LightOff.SetActive(true);
             salle.lumiere = false;
         }
