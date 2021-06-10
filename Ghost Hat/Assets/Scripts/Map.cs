641a6642fea73502c4a6705c6df0b7171ba9c683
Alexandre Le Bacquer <alexandre.lbac@gmail.com>
2021-06-10 17:12:51 +0200

    Revert "carte level"
    This reverts commit 1b8ad87159ba814d782a116dd04575c709054fd3.

--- Ghost Hat/Assets/Scripts/Map.cs
+++ Ghost Hat/Assets/Scripts/Map.cs
@@ -1,29 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Map : MonoBehaviour
 {
-    public GameObject button;
-    public Image carte;
+    void Start()
+    {
+        
+    }
 
     void Update()
     {
-        Debug.Log(carte.rectTransform.position);
-        if(GameManager.State == GameState.MainMenu)
+        Debug.Log("a");
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            if (Input.GetAxis("Mouse ScrollWheel") > 0f && carte.rectTransform.position.x <= 3298)
-            {
-                Debug.Log(transform.position);
-                carte.rectTransform.position = new Vector2(carte.rectTransform.position.x + 40, carte.rectTransform.position.y);
-                button.transform.position = new Vector2(button.transform.position.x + 40, button.transform.position.y);
-            }
-            else if (Input.GetAxis("Mouse ScrollWheel") < 0f && carte.rectTransform.position.x >= -1341)
-            {
-                carte.rectTransform.position = new Vector2(carte.rectTransform.position.x - 40, carte.rectTransform.position.y);
-                button.transform.position = new Vector2(button.transform.position.x - 40, button.transform.position.y);
-            }
+            transform.Rotate(Vector3.forward * 6);
+            Debug.Log("b");
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            transform.Rotate(Vector3.back * 6);
         }
     }
 }
